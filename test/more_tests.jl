bdd8a4ef7fbde0b70825445d9c5ed7696eefbeb9
fredo <frederic.testard@gmail.com>
2014-08-23 13:41:47 +0200

    added tests for plot

--- test/more_tests.jl
+++ test/more_tests.jl
@@ -15,4 +15,17 @@ m.@deriv_rule .+(x::Real   , y::AbstractArray)    x     (a=0.; for x in ds ; a+=
 
 ex = m.rdiff( :( sum( x .+ [1., 2.]) ), x=1.)
 
+######### plot  ############
+
+ex = quote 
+	a = 0.
+	for i in 1:10
+		a += i
+	end
+	a
+end
+
+m.plot( m.tograph(ex))
+
+
 
