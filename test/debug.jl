faa9b1853439fca8731a16e0da661eb70330bdbc
fredo <frederic.testard@gmail.com>
2015-05-04 19:17:18 +0200

    regex on expr (WIP)

--- test/debug.jl
+++ test/debug.jl
@@ -56,8 +56,6 @@
 
         Expr(:call, mexpr( mt ), Any[ valueof(x,n) for x in n.parents[2:end] ]...)
 
-
-
 ############## external symbols resolution  #########################
     reload("ReverseDiffSource") ; m = ReverseDiffSource
 
@@ -105,7 +103,6 @@
 
         t = Abcd.Abcd2.probe2
 
-
 ###################### issue #8   ######################################
     reload("ReverseDiffSource") ; m = ReverseDiffSource
 
@@ -151,15 +148,15 @@
 
 ##############   loops in functions  #################################
 
-function tt(x)
-    a = zeros(2)
-    for i in 1:2
-        a[i] = x
+    function tt(x)
+        a = zeros(2)
+        for i in 1:2
+            a[i] = x
+        end
+        sum(a)
     end
-    sum(a)
-end
 
-# (f::Function, sig0::Tuple; order::Int=1, evalmod=Main, debug=false, allorders=true)
+    # (f::Function, sig0::Tuple; order::Int=1, evalmod=Main, debug=false, allorders=true)
     f=tt;sig0=(1.,);order=1;evalmod=Main;debug=false;allorders=true
 
     sig = map( typeof, sig0 )
@@ -193,24 +190,125 @@ end
     mes = repr(streamline(mex))
     dump(mex.args[5].args[2].args[1])
 
-mex
-:(begin  # In[22], line 2:
-        a = zeros(2) # line 3:
+    mex2 = streamline(mex)
+    :(begin 
+        a = zeros(2)
         GenSym(0) = colon(1,2)
-        #s132 = start(GenSym(0))
-        unless !(done(GenSym(0),#s132)) goto 1
+        #s136 = start(GenSym(0))
+        unless !(done(GenSym(0),#s136)) goto 1
         2: 
-        GenSym(1) = next(GenSym(0),#s132)
-        i = tupleref(GenSym(1),1)
-        #s132 = tupleref(GenSym(1),2) # line 4:
+        GenSym(1) = next(GenSym(0),#s136)
+        i = getfield(GenSym(1),1)
+        #s136 = getfield(GenSym(1),2)
         setindex!(a,x,i)
         3: 
-        unless !(!(done(GenSym(0),#s132))) goto 2
+        unless !(!(done(GenSym(0),#s136))) goto 2
         1: 
-        0:  # line 6:
+        0: 
         return sum(a)
     end)
 
+    mex2.args[4]
+
+:(begin 
+        a = zeros(2)
+        GenSym(0) = colon(1,2)
+        _s1 = start(GenSym(0))
+        # unless !(done(GenSym(0),_s1)) goto 1
+2:         GenSym(1) = next(GenSym(0),_s1)
+        i = getfield(GenSym(1),1)
+        _s1 = getfield(GenSym(1),2)
+        setindex!(a,x,i)
+# 3:         unless !(!(done(GenSym(0),_s1))) goto 2
+1: 0:         return sum(a)
+    end)
+
+    dump( mex2.args[4] )
+    dump( :( gotoifnot (done(lst, s1),1 ) ) )
+
+    rexp = quote
+        GenSym(regexp"(\d+)\k{g0}") = regexp"(.*)\k{range}"
+        regexp"(\d+)\k{iter}" = start(GenSym(regexp"\g{g0}"))
+        gotoifnot( !(done(GenSym(regexp"\g{g0}"), regexp"\g{iter}" )) , 1 )
+        2: GenSym(regexp"(\d+)\k{g1}") = next(GenSym(regexp"\g{g0}"), regexp"\g{iter}")
+        i = getfield(GenSym(1),1)
+        regexp"\g{iter}" = getfield(GenSym(regexp"\g{g0}"),2)
+        setindex!(a,x,i)
+        3: gotoifnot( !(!(done(GenSym(regexp"\g{g0}"), regexp"\g{iter}"))) , 2 )
+    end
+
+    e2s(streamline(rexp))
+
+    dump( mex2.args[10] )
+    ln = mex2.args[10]
+    isa(ln, LabelNode)
+    isa(ln, Expr)
+    repr(ln)
+
+
+    dump(:( regexp"(\d+)" ))
+   dump(:( 0: ))
+
+    function e2s(ex::Expr)
+        ex.head == :macrocall && ex.args[1] == Symbol("@regexp_str") && return(ex.args[2])
+
+        if ex.head == :call && ex.args[1] == :gotoifnot
+            es = "↑:gotoifnot"
+            ra = 2:length(ex.args)
+        else
+            es = "↑$(ex.head)"
+            ra = 1:length(ex.args)
+        end
+        for a in ex.args[ra]
+            es *= "→" * e2s(a)
+        end
+        return es * "↓"
+    end
+
+    function e2s(thing)
+        res = repr(thing)
+        # now escape regex special characters
+        i = start(res)
+        res2 = ""
+        while !done(res,i)
+            c, j = next(res,i)
+            c in ['(', ')', '+', '*', '.', '$', '^', '[', ']', '|'] && res2 *= string('\\')
+            res2 *= string(c)
+            i = j
+        end
+        res2
+    end
+    e2s(:(4+bcd))
+
+    rexp2 = Regex(e2s(streamline(rexp)))
+    tr = Regex(".*")
+    dump(tr)
+
+    match(rexp2)
+
+    rexp = quote
+        regexp"\("
+        GenSym(regexp"(\d+)") = regexp"(.*)"
+        _s136 = start(GenSym(regexp"\g{0}"))
+        gotoifnot( !(done(GenSym(0),_s136)), 1)
+    end
+    e2s(streamline(rexp))
+
+    e2s(mex2)
+    dump(mex2.args)
+
+    ### 
+    function loopextract(ex::Vector{Expr})
+        for e in ex
+            
+
+
+        return (before, idx, inside, after)
+    end    
+
+
+
+    ###
     pr = r"""
     (.*)\n
     \W+ (GenSym\(\d+\))\ =\ (.*)\n
